f197790c284940f73ec1515c862e923087ae2c3f
varunbhardwaj22 <varunbhardwaj2299@gmail.com>
2021-11-18 11:13:36 +0530

    Syncronisation-Implicit,explicit,fluent waits

---
+++ SeleniumIntrorepository/SeleniumIntroduction/Assignments/ExplicitWait.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"/home/varun.bhardwaj@npci.org.in/Downloads/chromedriver_linux64/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.itgeared.com/demo/1506-ajax-loading.html");
+		driver.findElement(By.linkText("Click to load get data via Ajax!")).click();
+		WebDriverWait w = new WebDriverWait(driver, 5);
+		String text = w.until(ExpectedConditions.visibilityOfElementLocated(By.id("results"))).getText();
+		System.out.println(text);
+	}
+
+}
