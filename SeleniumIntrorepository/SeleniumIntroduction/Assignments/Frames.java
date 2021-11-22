f348f314c5b696b84f365b2f5325d3aea158ae88
varunbhardwaj22 <varunbhardwaj2299@gmail.com>
2021-11-22 15:33:20 +0530

    Frames Assignment

---
+++ SeleniumIntrorepository/SeleniumIntroduction/Assignments/Frames.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Frames {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver",
+				"/home/varun/Downloads/chromedriver_linux64/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/");
+		
+		driver.findElement(By.linkText("Nested Frames")).click();
+		driver.switchTo().frame("frame-top");
+		driver.switchTo().frame("frame-middle");
+		System.out.println(driver.findElement(By.id("content")).getText());
+		
+	}
+
+}
