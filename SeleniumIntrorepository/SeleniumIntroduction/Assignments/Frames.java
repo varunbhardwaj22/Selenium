33924cf5b634bab9282dbd6a3e7960e65a2cf8fd
varunbhardwaj22 <varunbhardwaj2299@gmail.com>
2021-11-23 19:13:40 +0530

    Scopes advance selenium

--- SeleniumIntrorepository/SeleniumIntroduction/Assignments/Frames.java
+++ SeleniumIntrorepository/SeleniumIntroduction/Assignments/Frames.java
@@ -7,17 +7,16 @@ public class Frames {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver",
-				"/home/varun/Downloads/chromedriver_linux64/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/home/varun/Downloads/chromedriver_linux64/chromedriver");
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/");
-		
+
 		driver.findElement(By.linkText("Nested Frames")).click();
 		driver.switchTo().frame("frame-top");
 		driver.switchTo().frame("frame-middle");
 		System.out.println(driver.findElement(By.id("content")).getText());
-		
+
 	}
 
 }
---
+++ SeleniumIntrorepository/SeleniumIntroduction/src/Advanced/Scope.java
@@ -0,0 +1,56 @@
+package Advanced;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scope {
+	public static void main(String args[])
+	{
+		System.setProperty("webdriver.chrome.driver", "/home/varun.bhardwaj@npci.org.in/Downloads/chromedriver_linux64/chromedriver");
+		
+		WebDriver driver =new ChromeDriver();
+
+		driver.get("http://qaclickacademy.com/practice.php/");
+		
+		//Count of links on the page
+		System.out.println(driver.findElements(By.tagName("a")).size());
+		
+		WebElement footdriver = driver.findElement(By.id("gf-BIG"));  //Limiting web driver scope
+		
+		//Count of links in the footer section
+		System.out.println(footdriver.findElements(By.tagName("a")).size());
+		
+		WebElement columndriver = driver.findElement(By.xpath("//table/tbody/tr/td[1]/ul")); //Limiting scope 
+
+		//Count of links in the first column in footer section
+		System.out.println(columndriver.findElements(By.tagName("a")).size());
+		
+		//Click on each link of the column and check if they are working
+		String clickonlinktab = Keys.chord(Keys.CONTROL,Keys.ENTER);
+		
+		//Open all the tabs
+		for(int i=1;i<columndriver.findElements(By.tagName("a")).size();i++)
+		{
+		 	columndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlinktab);
+		}
+		
+		Set<String> windows = driver.getWindowHandles(); 
+
+		Iterator<String>it = windows.iterator();
+		
+		while(it.hasNext()) {
+			driver.switchTo().window(it.next());
+			System.out.println(driver.getTitle());
+			}
+		
+		
+		
+	}
+
+}
