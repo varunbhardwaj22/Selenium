90f2c7df4c3ba2e994c4b265dc3bad98dfe40d15
varunbhardwaj22 <varunbhardwaj2299@gmail.com>
2021-11-25 18:24:13 +0530

    Assignment Automatic drop down

---
+++ SeleniumIntrorepository/SeleniumIntroduction/Assignments/AutomaticDropdown.java
@@ -0,0 +1,37 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AutomaticDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+
+// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver",
+				"/home/varun/Downloads/chromedriver_linux64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("http://qaclickacademy.com/practice.php");
+
+		driver.findElement(By.id("autocomplete")).sendKeys("ind");
+
+		Thread.sleep(2000);
+
+		driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+
+		driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+
+		System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value"));
+
+	}
+
+}
