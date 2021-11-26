565340cdb9085052aaebc1a79ee507e655a56888
varunbhardwaj22 <varunbhardwaj2299@gmail.com>
2021-11-26 16:03:15 +0530

    Soft assestions,Cookies,Screenshot and url handling

---
+++ SeleniumIntrorepository/SeleniumIntroduction/src/CookiesAndScreenshot.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//Need to add this dependency for using FileUtils 
+//<dependency>
+//<groupId>org.apache.directory.studio</groupId>
+//<artifactId>org.apache.commons.io</artifactId>
+//<version>2.4</version>
+//</dependency>
+public class CookiesAndScreenshot {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"/home/varun/Downloads/chromedriver_linux64/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().deleteCookieNamed("session");  //To delete a particular cookie
+		driver.get("https://www.google.co.in/");
+		
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src,new File("/home/varun.bhardwaj@npci.org.in/Desktop/SeleniumScreenshot.png"));
+		
+	}
+
+}
